3ec503fa93045025e548dae18dc76e2c647d51f3
Maxik73rus <myasoed73@mail.ru>
2020-06-24 19:18:02 +0300

    готово,проверяйте!

--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import PageDetail from './PageDetail/PageDetail'
 import CssModulePage from './CssModulePage/CssModulePage'
+import Page404 from './Page404/Page404'
+import PageHome from './PageHome/PageHome'
 
 function Routes () {
   return (
     <Switch>
+      <Route exact path='/' component={PageHome} />
       <Route path='/detail' component={PageDetail} />
       <Route path='/css-module' component={CssModulePage} />
+      <Route path='/page404' component={Page404} />
     </Switch>
   )
 }
-
 export default Routes
